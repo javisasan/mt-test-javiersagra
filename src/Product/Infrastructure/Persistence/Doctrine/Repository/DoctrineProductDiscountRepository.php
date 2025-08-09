9a6cbff3bd72803a01acf44c83c128d7089fd1c1
Javi S <javisasan@gmail.com>
2025-08-09 21:12:23 +0200

    MR-004: Use caching instead of database

--- src/Product/Infrastructure/Persistence/Doctrine/Repository/DoctrineProductDiscountRepository.php
+++ src/Product/Infrastructure/Persistence/Doctrine/Repository/DoctrineProductDiscountRepository.php
@@ -4,28 +4,51 @@ namespace App\Product\Infrastructure\Persistence\Doctrine\Repository;
 
 use App\Product\Domain\Entity\ProductDiscount;
 use App\Product\Domain\Repository\ProductDiscountRepositoryInterface;
+use App\SharedKernel\Application\Cache\Adapter\CacheAdapterInterface;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 
 class DoctrineProductDiscountRepository extends ServiceEntityRepository  implements ProductDiscountRepositoryInterface
 {
-    private EntityManagerInterface $entityManager;
+    private const CACHE_PRODUCT_DISCOUNT_KEY = 'product_discount_';
 
-    public function __construct(ManagerRegistry $registry)
-    {
+    private EntityManagerInterface $entityManager;
+    private CacheAdapterInterface $cache;
+
+    public function __construct(
+        ManagerRegistry $registry,
+        CacheAdapterInterface $cache
+    ) {
         parent::__construct($registry, ProductDiscount::class);
         $this->entityManager = $this->getEntityManager();
+        $this->cache = $cache;
     }
 
     public function findOneBySku(string $sku): ?ProductDiscount
     {
-        return $this->findOneBy(['productSku' => $sku]);
+        $cacheKey = $this->getCacheKey($sku);
+        $productDiscount = $this->cache->get($cacheKey);
+
+        if ($productDiscount !== false) {
+            return $productDiscount;
+        }
+
+        $productDiscount = $this->findOneBy(['productSku' => $sku]);
+
+        $this->cache->set($cacheKey, $productDiscount);
+
+        return $productDiscount;
     }
 
     public function save(ProductDiscount $productDiscount): void
     {
         $this->entityManager->persist($productDiscount);
         $this->entityManager->flush();
+    }
+
+    private function getCacheKey(string $key): string
+    {
+        return self::CACHE_PRODUCT_DISCOUNT_KEY . $key;
     }
 }
